5c8e22e4367cb74216a804ffdcc6d6c4b05f37d9
sammiefrog <sammiefrog@live.com>
2020-03-11 16:29:57 -0500

    saving to local storage as object

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# workday-planner
+# Workday Planner
 
 ## Live Link
 https://sammiefrog.github.io/workday-planner/
+
+## Description
+This is a daily planner for a typical work day. It is coded for 8am to 4pm, my ideal workday, but it can be easily adjusted. The input field can save to local storage and be deleted when a task is completed. Also, the current hour is shown in yellow, future hours are blue, and past hours turn red.
+
+## Technology Stack
+HTML, Bootstrap, JQuery, Moment.js
+
+## Contributors
+@sammiefrog
+
+## Usage
+To track daily tasks during your work day, you can save and delete tasks, and the time slots turn red as that hour passes.
+
+## Credits
+www.w3schools.com 
+
+## Contact
+* #### Name: Sammantha Sasenick (@sammiefrog)
+* #### Email: [sammiefrog@live.com](sammiefrog@live.com)
+* #### LinkedIn: www.linkedin.com/in/sammantha-sasenick412
+
+## License
+MIT
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var timeBlocks = $('#time-blocks');
 // var workDayHours = ['8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm'];
 
 //currently in use
-var savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+// var savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 var workDayHours = [8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 //i use this for the second way of showing my rows
@@ -31,7 +31,7 @@ function createTimeBlocks() {
             '</div>' +
             '<input type="text" class="form-control" value="" id="' + workDayHours[i] + '">' +
             '<div class="input-group-append">' +
-            '<button class="btn btn-primary saveButton" data-time="' + workDayHours[i] + '"><i class="fa fa-save"></i></button>' +
+            '<button class="btn btn-success saveButton" data-time="' + workDayHours[i] + '"><i class="fa fa-save"></i></button>' +
             '</div>' +
             '</div>';
         blocks += hourBlocks;
@@ -41,29 +41,47 @@ function createTimeBlocks() {
 
 //this click is used for either method of showing rows^
     $('.saveButton').on("click", function() {
-        // var timeSlot = event.currentTarget.getAttribute('data-time');
-        var timeSlot = $(this).attr('data-time');
-        var userTask = $(':input').val();
-        // var userTask1 = $('#8').val();
-        // var userTask2 = $('#9').val();
-        // var userTask3 = $('#10').val();
-        // var userTask4 = $('#11').val();
-        // var userTask = $(input).val();
-        // var userTask = $('INPUT').val();
-        console.log(timeSlot);
-        console.log(userTask);
-        // console.log(userTask1);
-        // console.log(userTask2);
-        // console.log(userTask3);
-        // console.log(userTask4);
-        // savedTasks.push(userTask);
-        savedTasks[timeSlot] = userTask;
-        localStorage.setItem("tasks", JSON.stringify(savedTasks));
-        localStorage.getItem("tasks");
 
+        var timeSlot = $(this).attr('data-time');
+        var userTask = $(`#${timeSlot}`).val();
+        localStorage.setItem(timeSlot, userTask);
+        
+        // var savedTask = {
+        //     timeSlot : userTask
+        // }
+
+        // localStorage.setItem('savedTasks', savedTask);
+        // localStorage.getItem(JSON.parse('savedTasks'));
+        
+
+        // var savedTasks = localStorage.setItem(timeSlot, userTask);
+        // localStorage.getItem(JSON.parse(timeSlot, userTask));
+
+        // things to search/try..
+        // 1. saving unique key pairs to local storage
+        // 2. retrieving key value pairs from local storage after save
+        // var myObj = localStorage.getItem(timeSlot, userTask);
+
+        // if(myObj) {
+        //     myObj = JSON.parse(myObj); 
+        // } 
+        // else {
+        //     myObj = {};
+        // }
+        // appendNewValue();
+        // function appendNewValue(timeSlot, userTask){
+        //     myObj[timeSlot] = userTask;
+        //     localStorage.setItem(timeSlot, JSON.stringify(myObj));
+        //             /* Saves data immediately. Instead of saving every time, you can
+        //             also add this persistence feature to the `(before)unload` handler. */
+        // }
     });
 
-//showing up green at 8am....
+
+
+    
+
+//colorizing the input fields!!!!!
 for (i = 0; i < workDayHours.length; i++) {
 
     if (currentHour === workDayHours[i]) {
@@ -85,54 +103,6 @@ createTimeBlocks();
 //don't delete this dummy
 });
 
-//pseudocode
-//create an input line for each hour of the work day
-//prepend the hour to each input
-//append a save button to each input
-//on click of the save button save it to localStorage
-//also - keep it displayed there when it is saved
-//second way
-    // $.each(currentTime, function(key, value) {
-    
-    //     var hourBlocks = "" +
-    //     '<div class="input-group mb-3">' +
-    //     '<div class="input-group-prepend">' +
-    //     '<span class="input-group-text">' + value + '</span>' +
-    //     '</div>' +
-    //     '<input type="text" class="form-control inputVal" id="color userInput" value="">' +
-    //     '<div class="input-group-append">' +
-    //     '<button class="btn btn-primary saveButton" value="i" data-time="' + key + '"><i class="fa fa-save"></i></button>' +
-    //     '</div>' +
-    //     '</div>';
-    // blocks += hourBlocks;
-    // });
-    // timeBlocks.append(blocks);
-    
 
-//third way via josh - doesnt work on my format?
-// Object.keys(savedTasks).forEach(function(time) {
-//     var hourBlocks = "" +
-//         '<div class="input-group mb-3">' +
-//         '<div class="input-group-prepend">' +
-//         '<span class="input-group-text">' + time + '</span>' +
-//         '</div>' +
-//         `<input type="text" style="background-color: ${setColor(time)}" class="form-control" id="input-${time}" value="${savedTasks[time]}"/>` +
-//         '<div class="input-group-append">' +
-//         '<button class="btn btn-primary saveButton" data-time="'  + time + '"><i class="fa fa-save"></i></button>' +
-//         '</div>' +
-//         '</div>';
-//     blocks += hourBlocks;
-// });
-
-    // $(".saveButton").on("click", function(event) {
-    // var timeSlot = event.currentTarget.getAttribute('data-time');
-    // var userTask = $("#userInput-" + timeSlot).val();
-    // // savedTasks.push(userTask);
-    // savedTasks[timeSlot] = userTask;
-    // localStorage.setItem("tasks", JSON.stringify(savedTasks));
-
-    // });
-
-
-
-// console.log(savedTasks);
+        // savedTasks[timeSlot] = userTask;
+        // localStorage.setItem("tasks", JSON.stringify(savedTasks));
