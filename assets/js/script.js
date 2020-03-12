8fd917679f7af93def8e41cc43d8ec4465f3bce0
sammiefrog <sammiefrog@live.com>
2020-03-12 10:20:25 -0500

    trying to get saves to display on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,19 @@ eDisplayMoment.text(nowMoment);
 
 //where the time blocks go in html
 var timeBlocks = $('#time-blocks');
-// var blockContainer = $('.container');
-
-// var workHours = {"8AM": "", "9AM": "", "10AM": "", "11AM": "", "12PM": "", "1PM": "", "2PM": "", "3PM": "", "4PM": ""};
-// var workDayHours = ['8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm'];
 
 //currently in use
-// var savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-var userTask;
-var timeSlot;
-const savedTasks = localStorage.getItem(timeSlot) || {};
+// var userTask;
+// var timeSlot;
+var taskData = {};
 
-// var savedTasks = localStorage.getItem(timeSlot);
+var savedTasks = JSON.parse(localStorage.getItem("saves")) || taskData;
+
+console.log(savedTasks);
 
 var workDayHours = [8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 //i use this for the second way of showing my rows
-// var currentTime = {8: "8AM", 9: "9AM", 10: "10AM", 11: "11AM", 12: "12PM", 13: "1PM", 14: "2PM", 15: "3PM", 16: "4PM"}
 
 function createTimeBlocks() {
     var blocks = "";
@@ -35,15 +31,13 @@ function createTimeBlocks() {
             '<span class="input-group-text">' + workDayHours[i]+ ":00" +
              '</span>' +
             '</div>' +
-            '<input type="text" class="form-control" id="' + workDayHours[i] + '" value=""/>' +
+            '<input type="text" class="form-control" id="' + workDayHours[i] + '"/>' +
             '<div class="input-group-append">' +
             '<button class="btn btn-success saveButton" data-time="' + workDayHours[i] + '"><i class="fa fa-save"></i></button>' +
             '</div>' +
             '</div>';
         blocks += hourBlocks;
-        for (var j = 0; j < workDayHours.length; j++) {
-            $('#' + workDayHours[i]).text(timeSlot);
-        }
+            $('#' + workDayHours[i]).text(savedTasks);
     }
     timeBlocks.append(blocks);
 
@@ -56,39 +50,12 @@ function createTimeBlocks() {
         var timeSlot = $(this).attr('data-time');
         var userTask = $(`#${timeSlot}`).val();
 
+        savedTasks[timeSlot] = userTask;
 
-        localStorage.setItem(timeSlot, userTask);
+        localStorage.setItem("saves", JSON.stringify(savedTasks));
 
         console.log(localStorage);
-        // var savedTask = {
-        //     timeSlot : userTask
-        // }
 
-        // localStorage.setItem('savedTasks', savedTask);
-        // localStorage.getItem(JSON.parse('savedTasks'));
-        
-
-        // var savedTasks = localStorage.setItem(timeSlot, userTask);
-        // localStorage.getItem(JSON.parse(timeSlot, userTask));
-
-        // things to search/try..
-        // 1. saving unique key pairs to local storage
-        // 2. retrieving key value pairs from local storage after save
-        // var myObj = localStorage.getItem(timeSlot, userTask);
-
-        // if(myObj) {
-        //     myObj = JSON.parse(myObj); 
-        // } 
-        // else {
-        //     myObj = {};
-        // }
-        // appendNewValue();
-        // function appendNewValue(timeSlot, userTask){
-        //     myObj[timeSlot] = userTask;
-        //     localStorage.setItem(timeSlot, JSON.stringify(myObj));
-        //             /* Saves data immediately. Instead of saving every time, you can
-        //             also add this persistence feature to the `(before)unload` handler. */
-        // }
     });
 
 
